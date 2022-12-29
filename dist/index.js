df00da476be898d294248ba686ad9777405eeb31
Stuart Whitlam <stuart.whitlam1@digital.homeoffice.gov.uk>
2022-12-29 15:27:50 +0000

    Refactor to externalise version yaml path

--- dist/index.js
+++ dist/index.js
@@ -16947,45 +16947,28 @@ const yaml = __nccwpck_require__(1917);
 
 async function run() {
 
-
-    const QA_VERSIONS_PATH = './environments/qa/versions.yaml';
-
-    const doc = yaml.load(fs.readFileSync(QA_VERSIONS_PATH, 'utf8'));
-
-    console.log(doc);
-    const repositoryNames = Object.keys(doc.versions);
-
-    repositoryNames.forEach((repositoryName) => {
-        console.dir(repositoryName);
-    });
-
     const inputs = getActionInputs([
-      { name: "github_token", options: { required: true } }
+        { name: "github_token", options: { required: true } },
+        { name: "version_file_path", options: { required: true } },
     ]);
+
+    const doc = yaml.load(fs.readFileSync(inputs.version_file_path, 'utf8'));
+    const repositoryNames = Object.keys(doc.versions);
 
     const tagsPromises = repositoryNames.map(async (repositoryName) => {
         const { data: tags } = await github.getOctokit(inputs.github_token).rest.repos.listTags({
             owner: github.context.payload.repository.owner.login,
             repo: repositoryName,
         });
-
-        // remove invalid semver tags
-        const validTags = tags.filter((tag) => semver.valid(tag.name));
-
-        // Sort the tags by version number and return the tag with the highest version number
-        return validTags.sort((a, b) => semver.rcompare(a.name, b.name))[0];
+        return tags.filter((tag) => semver.valid(tag.name)).sort((a, b) => semver.rcompare(a.name, b.name))[0];
     });
 
     const tags = await Promise.all(tagsPromises);
-
     tags.forEach((tag, index) => {
         doc.versions[repositoryNames[index]] = tag.name;
-        core.info(`Tag with highest version number for repository ${repositoryNames[index]}: ${tag.name}`);
     });
 
-    console.log(doc)
-    fs.writeFileSync(QA_VERSIONS_PATH, yaml.dump(doc));
-
+    fs.writeFileSync(inputs.version_file_path, yaml.dump(doc));
 }
 
 const getActionInputs = (variables) => {
